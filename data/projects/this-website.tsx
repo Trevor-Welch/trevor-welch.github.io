f1a46c6890b4e6d1844d7702ae9311fda83b7a4b
Trevor Welch <welch.trevor@yahoo.com>
2025-12-19 11:16:13 -0500

    Cleaning Up Code / Project Descriptions

--- data/projects/this-website.tsx
+++ data/projects/this-website.tsx
@@ -19,6 +19,9 @@ export const thisWebsite: Project = {
       <TagsBar tags={tags} date={year}/>
       <h3>Trying something new</h3>
       <p>There's a bit of magic going on behind the scenes, but it's mostly Next.js with thousands of lines of code and CSS being hosted for free on GitHub Pages. It is not perfect. It is not made to be easy to update. But it costs me nothing and it is fun to look at.</p>
+      <p>For the contact form, I created an endpoint on my DNS for <b>/api/contact/</b> that points to a Cloudflare Worker that will take the form data and send it to me for free using Brevo. I skipped on reCaptcha since it barely does anything to prevent spam these days and went with a honeypot and timer as I've seen less spam since implementing it.</p>
+      <p>I have Hotjar for some analytics and to get free user feedback.</p>
+      <p>The only thing I pay for is my domain name!</p>
       <StylizedButton 
         label='View on GitHub'
         href='https://github.com/trevor-welch/trevor-welch.github.io'
